052e75c2f3587ec15d49aef74f41f3983515e883
srh2006 <srh2006@live.com>
2018-11-01 11:18:54 -0700

    Completed Module

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -16,10 +16,13 @@ namespace GradeBook.GradeBooks
 
         public GradeBookType Type { get; set; }
 
-        public BaseGradeBook(string name)
+        public bool IsWeighted { get; set; }
+
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -108,20 +111,26 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int i = 0;
+            if (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled && IsWeighted)
+            {
+                i++;
+            }
+
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return (4 + i);
                 case 'B':
-                    return 3;
+                    return (3 + i);
                 case 'C':
-                    return 2;
+                    return (2 + i);
                 case 'D':
-                    return 1;
+                    return (1 + i);
                 case 'F':
-                    return 0;
+                    return (0 + i);
             }
-            return 0;
+            return (0 + i);
         }
 
         public virtual void CalculateStatistics()
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -6,7 +6,7 @@ namespace GradeBook.GradeBooks
 {
     class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook (string name) : base(name)
+        public StandardGradeBook (string name, bool isWeighted) : base(name,isWeighted)
         {
             Type = Enums.GradeBookType.Standard;
         }
